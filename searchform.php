4834bf4b7296c8c5b9b7569c6855895ee38d4bb0
Itai Lulu Koren <itailulu@gmail.com>
2015-01-07 11:47:28 +0200

    translate .po file and searchform.php

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
 ?>
 
 <form method="get" id="searchform" class="clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search the site...', 'ar2' ); ?>" <?php if ( '' != get_search_query() ) : ?>value="<?php echo get_search_query() ?>"<?php endif ?> />
-	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'ar2' ); ?>" />
+	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'חפש באתר...', 'ar2' ); ?>" <?php if ( '' != get_search_query() ) : ?>value="<?php echo get_search_query() ?>"<?php endif ?> />
+	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'חפש', 'ar2' ); ?>" />
 </form>
